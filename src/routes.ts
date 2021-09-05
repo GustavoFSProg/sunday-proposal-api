bdc7f02b25bc768e4fab4371d4d603e1bbe461e4
Gustavo <gustavo.prog40@gmail.com>
2021-09-05 14:01:52 -0300

    asdadf

--- src/routes.ts
+++ src/routes.ts
@@ -4,7 +4,7 @@ import userController from './controllers/userController'
 const routes = Router()
 
 routes.get('/', userController.getAll)
-routes.get('/get-email', userController.getByEmail)
+// routes.get('/get-email', userController.getByEmail)
 routes.post('/register', userController.register)
 routes.put('/update/:id', userController.update)
 routes.delete('/delete/:id', userController.deleteOne)
